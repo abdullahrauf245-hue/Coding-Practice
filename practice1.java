03f7e812456b0f20a07d6d3bcbf48efe48f94f16
MM.Abdullah <abdullahrauf245@gmail.com>
2026-04-01 22:37:32 +0500

    made a bit change

--- practice1.java
+++ practice1.java
@@ -34,14 +34,27 @@ public class practice1 {
         }
     }
 
+    static class duck extends Animal {
+        duck(String name) {
+            super(name);
+        }
+
+        @Override
+        void speak() {
+            System.out.println(name + " quacks!");
+        }
+    }
+
     // main directly in practice1, not inside another class
     public static void main(String[] args) {
         Animal a = new Animal("Generic Animal");
         Dog d = new Dog("Buddy");
         Cat c = new Cat("Whiskers");
+        duck u = new duck("Daffy");
 
         a.speak();
         d.speak();
         c.speak();
+        u.speak();
     }
 }
