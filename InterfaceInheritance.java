51cd6e185abdaee0a1202cc9027b7c03b2c74754
MM.Abdullah <abdullahrauf245@gmail.com>
2026-04-03 20:58:55 +0500

    Added a new daughter class

--- InterfaceInheritance.java
+++ InterfaceInheritance.java
@@ -11,8 +11,17 @@ public class InterfaceInheritance {
         double area() { return 0.5 * base * height; }
     }
 
+    static class Circle extends Shape {
+        double radius;
+        Circle(double r) { radius = r; }
+
+        @Override
+        double area() { return Math.PI * radius * radius; }
+    }
     public static void main(String[] args) {
         Triangle t = new Triangle(6, 4);
         System.out.println("Area: " + t.area());
+        Circle c = new Circle(5);
+        System.out.println("Area: " + c.area());
     }
 }
