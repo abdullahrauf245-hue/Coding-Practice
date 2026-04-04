e6fb5c02c577dcfb6a8d281ae6f270ba01aba915
MM.Abdullah <abdullahrauf245@gmail.com>
2026-04-05 00:11:51 +0500

    Nothing new

--- InterfaceInheritance.java
+++ InterfaceInheritance.java
@@ -35,13 +35,23 @@ public class InterfaceInheritance {
                     @Override
                     double area() { return length * breadth; }
     }
+    static class octagon extends Shape{
+        double side;
+        octagon(double s) { side = s; }
+
+        @Override
+        double area() { return 2 * (1 + Math.sqrt(2)) * side * side; }
+    }
     public static void main(String[] args) {
         Triangle t = new Triangle(6, 4);
         System.out.println("Area: " + t.area());
         Circle c = new Circle(5);
         Square s = new Square(3);
         Rectangle r = new Rectangle(4, 5);
+        octagon o = new octagon(3);
         System.out.println("Area: " + c.area());
-        
+        System.out.println("Area: " + s.area());
+        System.out.println("Area: " + r.area());
+        System.out.println("Area: " + o.area());
     }
 }
