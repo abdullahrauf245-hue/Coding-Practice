b2fc2393712bda35e37e9aa446d0b37b99baee36
MM.Abdullah <abdullahrauf245@gmail.com>
2026-04-04 16:13:53 +0500

    Adding new class

--- PolymorphismDemo.java
+++ PolymorphismDemo.java
@@ -18,12 +18,19 @@ public class PolymorphismDemo {
             System.out.println("Grad grade computed");
         }
     }
+    static class TeachingAssistant extends GraduateStudent {
+        @Override
+        void computeCourseGrade() {
+            System.out.println("TA grade computed");
+        }
+    }
 
     public static void main(String[] args) {
-        Student[] roster = new Student[3];
+        Student[] roster = new Student[4];
         roster[0] = new GraduateStudent();
         roster[1] = new UndergraduateStudent();
         roster[2] = new UndergraduateStudent();
+        roster[3] = new TeachingAssistant();
 
         for (Student s : roster) {
             s.computeCourseGrade();
