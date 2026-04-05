7cea0e50696262b84184d12d8e013f99c2cd82cf
MM.Abdullah <abdullahrauf245@gmail.com>
2026-04-05 21:57:59 +0500

    Learning Instanceof

---
+++ InstanceofExample.java
@@ -0,0 +1,22 @@
+public class InstanceofExample {
+    static class Animal {}
+    static class Dog extends Animal {
+        void fetch() { System.out.println("Fetching!"); }
+    }
+    static class Cat extends Animal {
+        void purr() { System.out.println("Purring!"); }
+    }
+
+    public static void main(String[] args) {
+        Animal a = new Dog();
+
+        System.out.println(a instanceof Dog);     // true
+        System.out.println(a instanceof Animal);  // true
+        System.out.println(a instanceof Cat);     // false
+
+        if (a instanceof Dog) {
+            Dog d = (Dog) a;
+            d.fetch();
+        }
+    }
+}
