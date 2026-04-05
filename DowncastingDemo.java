a6ca59312cde92b7dee698adbaef441e8e22f39e
MM.Abdullah <abdullahrauf245@gmail.com>
2026-04-05 23:50:22 +0500

    new class

--- DowncastingDemo.java
+++ DowncastingDemo.java
@@ -10,7 +10,11 @@ public class DowncastingDemo {
             System.out.println("Playing music in Car!");
         }
     }
-
+static class Bike extends Vehicle {
+        void ringBell() {
+            System.out.println("Ringing bell on Bike!");
+        }
+    }
     public static void main(String[] args) {
         Vehicle v = new Car();
 
@@ -19,6 +23,12 @@ public class DowncastingDemo {
         if (v instanceof Car) {
             Car c = (Car) v;
             c.playMusic();
+        }
+        if (v instanceof Bike) {
+            Bike b = (Bike) v;
+            b.ringBell();
+        } else {
+            System.out.println("v is not an instance of Bike, cannot ring bell.");
         }
     }
 }
