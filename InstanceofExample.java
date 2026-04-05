c6eff224120297f37fed18ae88003d83b8ca22fc
MM.Abdullah <abdullahrauf245@gmail.com>
2026-04-05 22:05:29 +0500

    added a new subclass

--- InstanceofExample.java
+++ InstanceofExample.java
@@ -6,13 +6,16 @@ public class InstanceofExample {
     static class Cat extends Animal {
         void purr() { System.out.println("Purring!"); }
     }
-
+static class Bird extends Animal {
+        void fly() { System.out.println("Flying!"); }
+    }
     public static void main(String[] args) {
         Animal a = new Dog();
 
         System.out.println(a instanceof Dog);     // true
         System.out.println(a instanceof Animal);  // true
         System.out.println(a instanceof Cat);     // false
+        System.out.println(a instanceof Bird);     // false
 
         if (a instanceof Dog) {
             Dog d = (Dog) a;
