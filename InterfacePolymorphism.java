af736d447251fd384c8e6b613a42e4fc5711844e
MM.Abdullah <abdullahrauf245@gmail.com>
2026-04-08 23:32:55 +0500

    New class

--- InterfacePolymorphism.java
+++ InterfacePolymorphism.java
@@ -9,8 +9,18 @@ public class InterfacePolymorphism {
         }
     }
 
+    static class piano implements Playable {
+        public void play() {
+            System.out.println("Playing the Piano!");
+        }
+    }
+
     public static void main(String[] args) {
         Guitar g = new Guitar();
         g.play();
+
+        piano p = new piano();
+        p.play();
+
     }
 }
